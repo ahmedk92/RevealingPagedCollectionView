24c040799394dd282b783b1b4985f43bb09222fb
Ahmed Khalaf <akhalaf@arabia-it.com>
2018-07-02 16:17:53 +0200

    basic implementation

--- RevealingCollectionView/ViewController.swift
+++ RevealingCollectionView/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let colors = [UIColor.red, .green, .blue]
+let cellCount = 10
+let pageWidthRatio: CGFloat = 0.9
+
+class Cell: UICollectionViewCell {
+    @IBOutlet var bgView: UIView!
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet private var collectionView: UICollectionView!
+    @IBOutlet private var secretScrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.isPagingEnabled = true
+        
+        collectionView.addGestureRecognizer(secretScrollView.panGestureRecognizer)
+        collectionView.panGestureRecognizer.isEnabled = false
+        
+        collectionView.contentInset = UIEdgeInsets.init(top: 0, left: (self.view.frame.size.width - pageWidthRatio * self.view.frame.size.width)/2, bottom: 0, right: (self.view.frame.size.width - pageWidthRatio * self.view.frame.size.width)/2)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        secretScrollView.contentSize = CGSize.init(width: collectionView.frame.size.width * pageWidthRatio * CGFloat(cellCount), height: collectionView.frame.size.height)
+        return cellCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Cell
+        cell.bgView.backgroundColor = colors[indexPath.row % colors.count]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size.applying(.init(scaleX: pageWidthRatio, y: 1))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == secretScrollView {
+            var offset = secretScrollView.contentOffset
+            offset.x -= collectionView.contentInset.left
+            collectionView.contentOffset = offset
+        }
+    }
 
 }
 
